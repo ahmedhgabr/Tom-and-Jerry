bba927a3d11f58f6159128e0a22680727a5a57eb
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-06-05 02:15:17 +0300

    feat: profile info

---
+++ app/src/main/java/com/example/testdeclarative/composable/component/ProfileInfo.kt
@@ -0,0 +1,150 @@
+package com.example.testdeclarative.composable.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.testdeclarative.R
+import com.example.testdeclarative.composable.view.TomAccount
+import com.example.testdeclarative.ui.theme.IBM
+import com.example.testdeclarative.ui.theme.fontColor_White
+import com.example.testdeclarative.ui.theme.profile_info
+
+
+@Composable
+fun ProfileInfo(imageId: Int, name: String, bio: String, modifier: Modifier = Modifier) {
+    Box {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxWidth()
+                .background(profile_info)
+                .padding(top = 16.dp, bottom = 10.dp)
+        ) {
+            Image(
+                painter = painterResource(id = imageId),
+                contentDescription = "profile picture",
+                modifier = Modifier
+                    .size(width = 64.dp, height = 64.dp)
+                    .clip(shape = RoundedCornerShape(12.dp))
+            )
+            Text(
+                text = name,
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.White,
+                    fontFamily = IBM
+                ),
+                color = fontColor_White,
+                modifier = Modifier
+                    .padding(top = 6.dp)
+                    .size(width = 36.dp, height = 27.dp)
+            )
+            Text(
+                text = bio,
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White.copy(alpha = 0.8f),
+                    fontFamily = IBM
+                ),
+                color = fontColor_White,
+                modifier = Modifier.size(width = 113.dp, height = 18.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .size(width = 111.dp, height = 40.dp)
+                    .padding(horizontal = 12.dp, vertical = 6.dp)
+                    .clip(shape = RoundedCornerShape(18.dp))
+                    .background(color = Color.White.copy(alpha = 0.12f))
+            ) {
+                Text(
+                    text = "Edit foolishness",
+                    style = TextStyle(
+                        fontSize = 10.sp,
+                        color = Color.White,
+                        fontFamily = IBM,
+                        fontWeight = FontWeight.Medium,
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                )
+            }
+        }
+//        BackgroundContainer()
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProfileInfo() {
+    ProfileInfo(R.drawable.tom_profile, "Tom", "specializes in failure!")
+}
+
+@Composable
+fun BackgroundContainer(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .requiredWidth(width = 360.dp)
+            .requiredHeight(height = 242.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .rotate(degrees = -32.2f)
+                .background(
+                    brush = Brush.linearGradient(
+                        0f to Color.White,
+                        0.41f to Color.White,
+                        start = Offset(401.15f, 487.05f),
+                        end = Offset(201.64f, -355.26f)
+                    )
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .rotate(degrees = 15.92f)
+                .background(
+                    brush = Brush.linearGradient(
+                        0f to Color.White,
+                        0.41f to Color.White,
+                        start = Offset(323.85f, 365.13f),
+                        end = Offset(162.78f, -266.33f)
+                    )
+                )
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xff226993))
+        )
+    }
+}
--- app/src/main/java/com/example/testdeclarative/composable/view/TomAccount.kt
+++ app/src/main/java/com/example/testdeclarative/composable/view/TomAccount.kt
@@ -6,33 +6,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.testdeclarative.R
+import com.example.testdeclarative.composable.component.ProfileInfo
 import com.example.testdeclarative.ui.theme.IBM
 import com.example.testdeclarative.ui.theme.bg_Account
 import com.example.testdeclarative.ui.theme.fontColor_White
@@ -46,101 +29,21 @@ fun TomAccount() {
             .background(bg_Account)
     ) {
         ProfileInfo(R.drawable.tom_profile, "Tom", "specializes in failure!")
+
+        Box(
+            modifier = Modifier
+                .background(color = Color(0xFFEEF4F6))
+                .clip(shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp))
+        ){
+
+        }
     }
 }
 
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 fun PreviewTomAccount() {
     TomAccount()
 }
 
-
-@Composable
-fun ProfileInfo(imageId: Int, name: String, bio: String, modifier: Modifier = Modifier) {
-    Box {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier
-                .fillMaxWidth()
-                .background(profile_info)
-                .padding(top = 16.dp, bottom = 10.dp)
-        ) {
-            Image(
-                painter = painterResource(id = imageId),
-                contentDescription = "profile picture",
-                modifier = Modifier
-                    .size(width = 64.dp, height = 64.dp)
-                    .clip(shape = RoundedCornerShape(12.dp))
-            )
-            Text(
-                text = name,
-                fontSize = 18.sp,
-                fontWeight = FontWeight(500),
-                color = fontColor_White,
-                modifier = Modifier
-                    .padding(top = 6.dp)
-                    .size(width = 36.dp, height = 27.dp)
-            )
-            Text(
-                text = bio,
-                fontSize = 12.sp,
-                fontWeight = FontWeight(400),
-                fontFamily = IBM,
-                color = fontColor_White,
-                modifier = Modifier.size(width = 113.dp, height = 18.dp)
-            )
-            Button(
-                onClick = { },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.White.copy(alpha = 0.12f)
-                ),
-                modifier = Modifier
-                    .size(width = 97.dp, height = 25.dp)
-            ) {
-                Text(
-                    text = "Edit foolishness",
-                    fontSize = 4.sp,
-                    fontWeight = FontWeight(500),
-                    color = fontColor_White
-                )
-            }
-        }
-        BackgroundContainer()
-    }
-
-}
-
-
-@Composable
-fun BackgroundContainer(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
-            .requiredWidth(width = 360.dp)
-            .requiredHeight(height = 242.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .rotate(degrees = -32.2f)
-                .background(brush = Brush.linearGradient(
-                    0f to Color.White,
-                    0.41f to Color.White,
-                    start = Offset(401.15f, 487.05f),
-                    end = Offset(201.64f, -355.26f))))
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .rotate(degrees = 15.92f)
-                .background(brush = Brush.linearGradient(
-                    0f to Color.White,
-                    0.41f to Color.White,
-                    start = Offset(323.85f, 365.13f),
-                    end = Offset(162.78f, -266.33f))))
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color(0xff226993)))
-    }
-}
